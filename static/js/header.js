3b9d143d6c62deefef2598df3eec04813748253e
ngphuan2017 <phuan2017@gmail.com>
2022-10-09 23:24:59 +0700

    slider finished

--- static/js/header.js
+++ static/js/header.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(window).scroll(function() {
-        if($(this).scrollTop() > 123.2){
+        if($(this).scrollTop() > 92.2){
             $('#nav').addClass('sticky');
         }else{
             $('#nav').removeClass('sticky');
