4706b491ca00b090560f35b1ad524e64d8083c13
ngphuan2017 <phuan2017@gmail.com>
2023-02-23 09:46:36 +0700

    ticket 1.2

--- static/js/backtop.js
+++ static/js/backtop.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if($(this).scrollTop() > 1000){
+        if($(this).scrollTop() > 600){
             $('#backtop').fadeIn();
         }else{
             $('#backtop').fadeOut();
         }
     });
     $('#backtop').click(function(){
-        $('html, body').animate({scrollTop: 0}, 1500);
+        $('html, body').animate({scrollTop: 0}, 2000);
+        return false;
     });
 });
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if(document.documentElement.scrollWidth > 800){
+//             alert(document.documentElement.scrollWidth);
+//         }else{
+//             alert(document.documentElement.scrollWidth);
+//         }
+//     });
+// });
