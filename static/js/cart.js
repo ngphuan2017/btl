001cc23f5b234f6cdf549fd4b72a3e70c2d250c5
ngphuan2017 <phuan2017@gmail.com>
2023-10-29 19:26:22 +0700

    v1

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,19 +1,35 @@
+$(document).ready(function () {
+    var quantitiy = 0;
+    $('.quantity-right-plus').click(function (e) {
+        e.preventDefault();
+        var quantity = parseInt($('#quantity').val());
+        $('#quantity').val(quantity + 1);
+    });
+    $('.quantity-left-minus').click(function (e) {
+        e.preventDefault();
+        var quantity = parseInt($('#quantity').val());
+        if (quantity > 0) {
+            $('#quantity').val(quantity - 1);
+        }
+    });
+});
+
 const btns = document.querySelectorAll('.js-add-cart');
 const cart = document.querySelector('.js-modal');
 const modalClose = document.querySelector('.js-modal-close');
 const modalContainer = document.querySelector('.js-modal-container');
-function showCart(){
+function showCart() {
     // alert('Modal opened successfully');
     cart.classList.add('js-modal-open');
 }
-function hideCart(){
+function hideCart() {
     cart.classList.remove('js-modal-open');
 }
-for (const btn of btns){
+for (const btn of btns) {
     btn.addEventListener('click', showCart);
 }
 modalClose.addEventListener('click', hideCart);
 cart.addEventListener('click', hideCart);
-modalContainer.addEventListener('click', function(event){
+modalContainer.addEventListener('click', function (event) {
     event.stopPropagation();
 });
